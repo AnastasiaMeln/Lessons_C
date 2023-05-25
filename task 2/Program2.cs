ac883e4110842d36048104f145a012f1df8b4759
Анастасия <anastasia@Air-Anastasia.Dlink>
2023-05-25 17:58:25 +0700

    Все задания

---
+++ task 2/Program2.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3 число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (max < b)
+    max = b;
+else if (max < c)
+{
+    max = c;
+}    
+Console.WriteLine($"Наибольшее число - {max}");
+ 
---
+++ task 4/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+int i = 2;
+do{
+    Console.Write($"{i} ");
+    i+=2;
+}
+while (i < n+1);
