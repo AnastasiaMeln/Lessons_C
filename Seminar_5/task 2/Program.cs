da495bba2b058651007ee7021064b525d3d19070
Анастасия <anastasia@Air-Anastasia.Dlink>
2023-06-09 23:42:16 +0700

    задания к пятому  семинару

---
+++ Seminar_5/task 2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 10);
+}
+
+int sumNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+        result = result + array[i];
+    return result;    
+}
+
+
+Console.Clear();
+int[] array = new int[10];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(sumNumbers(array));
---
+++ Seminar_5/task 3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+void InputArray(double[] array)
+{
+    int end = 30, begin = 10;
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+}
+
+Console.Clear();
+double[] array = new double[10];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+double maxValue = array.Max();
+double minValue = array.Min();
+Console.WriteLine($"{maxValue} - {minValue} = {Math.Round(maxValue - minValue, 2)}");
